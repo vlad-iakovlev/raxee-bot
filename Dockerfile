e8fbabe120518d192075adef17e5c0c5e2672aa7
Vladislav Iakovlev <vlad309523@gmail.com>
2026-08-02 03:51:02 +0200

    chore: support _FILE envs

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node:24-alpine AS base
 
+RUN apk add --no-cache bash
+
 WORKDIR /app
 COPY . .
 RUN npm ci --omit=dev
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,27 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Source: https://github.com/tiredofit/docker-alpine/blob/06ce99a4f2ddde26a0185c5782d78a057f16bd35/install/assets/functions/00-container#L1446-L1465
+transform_file_var() {
+  local variables
+  variables=$(echo "$@" | tr " " "\n")
+  for variable in $variables; do
+    if [ -v "${variable}"_FILE ] ; then
+      file_variable=${variable}_FILE
+      if [ ! -f "${!file_variable}" ] ; then
+        print_error "[transform_file_var] ${variable}_FILE set as environment variable, however file doesn't exist"
+        return 1
+      fi
+      export "${variable}"="$(cat "${!file_variable}")"
+    fi
+  done
+  unset file_variable
+  unset variables
+}
+
+transform_file_var \
+  DATABASE_URL \
+  BOT_TOKEN \
 
 echo "Applying database migrations..."
 npm run prisma -- migrate deploy
